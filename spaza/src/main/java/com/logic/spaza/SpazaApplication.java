68dc4ae45486674581349b3e98a01f21ee447047
KAUSHIK-S-13 <82277125+KAUSHIK-S-13@users.noreply.github.com>
2024-04-17 12:24:50 +0530

    Adding spaza/src/main/java/com/logic/spaza/SpazaApplication.java

---
+++ spaza/src/main/java/com/logic/spaza/SpazaApplication.java
@@ -0,0 +1,20 @@
+package com.logic.spaza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.context.annotation.Bean;
+import org.modelmapper.ModelMapper;
+
+@SpringBootApplication
+public class SpazaApplication {
+	@Bean
+	ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpazaApplication.class, args);
+	}
+
+}
